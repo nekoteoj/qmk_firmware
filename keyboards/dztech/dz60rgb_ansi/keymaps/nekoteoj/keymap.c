4d05af901deda3a600ba17d5e2449071c8a88d9d
Pisit Wajanasara <compisit1234@gmail.com>
2021-02-04 21:14:41 +0700

    nekoteoj's first keymap for dz60 ansi v2

---
+++ keyboards/dztech/dz60rgb_ansi/keymaps/nekoteoj/keymap.c
@@ -0,0 +1,75 @@
+#include QMK_KEYBOARD_H
+
+static bool is_lctl_pressed;
+static bool is_lalt_pressed;
+static bool is_lgui_pressed;
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT_60_ansi(
+        KC_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+        KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+        LT(3, KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_LSFT,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
+        KC_LCTL,        KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   MO(2),   KC_RCTL
+    ),
+    [1] = LAYOUT_60_ansi(
+        KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+        _______, _______, KC_UP,   _______, _______, _______, KC_CALC, _______, KC_INS,  _______, KC_PSCR, KC_SLCK, KC_PAUS, RESET,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP,          _______,
+        KC_MPRV,          KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, NK_TOGG, _______, _______, KC_END,  KC_PGDN,          KC_MNXT,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______
+    ),
+    [2] = LAYOUT_60_ansi(
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+        _______, RGB_TOG, _______, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_MOD, _______, _______, _______, RESET,
+        _______, _______, _______, _______, _______, _______, _______, _______, RGB_SPI, RGB_SPD, _______, _______,          _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______
+    ),
+    [3] = LAYOUT_60_ansi(
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+        _______, _______, KC_UP,   _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, _______, KC_BSPC,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,            _______,
+        _______, _______, _______,                            _______,                            KC_LEFT, KC_DOWN, KC_RGHT, _______
+    ),
+    [4] = LAYOUT_60_ansi(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,            RSFT_T(KC_SLSH),
+        _______, _______, _______,                            _______,                            KC_LEFT, KC_DOWN, KC_RGHT, _______
+    )
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_LCTL:
+            is_lctl_pressed = record->event.pressed;
+            return true;
+        case KC_LALT:
+            is_lalt_pressed = record->event.pressed;
+            return true;
+        case KC_LGUI:
+            is_lgui_pressed = record->event.pressed;
+            return true;
+        case KC_1:
+        case KC_F1:
+            if (record->event.pressed) {
+                if (is_lctl_pressed && is_lalt_pressed && is_lgui_pressed) {
+                    layer_move(0);
+                    return false;
+                }
+            }
+        case KC_2:
+        case KC_F2:
+            if (record->event.pressed) {
+                if (is_lctl_pressed && is_lalt_pressed && is_lgui_pressed) {
+                    layer_move(4);
+                    return false;
+                }
+            }
+        default:
+            return true;
+    }
+}
